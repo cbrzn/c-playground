e13260e5a51a0b6aa9552ff875d9aa988fd22c35
Cesar <cesarbrazon10@gmail.com>
2022-12-16 20:39:10 +0100

    chore: update dynamic lib with new naming + add readme

--- main.cpp
+++ main.cpp
@@ -7,21 +7,20 @@ extern "C" {
 }
 
 int main() {
-    const char* builder = create_builder();
-    const char* redirect_static_resolver = create_static_resolver(
-        "ens/add.eth",
-        "wrap://http/https://raw.githubusercontent.com/polywrap/wasm-test-harness/v0.2.1/wrappers/subinvoke/00-subinvoke/implementations/as"
-    );
+//    const char* builder = create_builder();
+//    const char* redirect_static_resolver = create_static_resolver(
+//        "ens/add.eth",
+//        "wrap://http/https://raw.githubusercontent.com/polywrap/wasm-test-harness/v0.2.1/wrappers/subinvoke/00-subinvoke/implementations/as"
+//    );
+//   add_static_resolver(builder, redirect_static_resolver);
+//    const char* resolver = build_resolver(builder);
 
-   add_static_resolver(builder, redirect_static_resolver);
-
-//    const char* resolver = create_resolver();
-    const char* resolver = build_resolver(builder);
+    const char* resolver = create_resolver();
 
     const char* client = create_client(resolver);
     struct Buffer result = invoke(
             client,
-            "ens/add.eth",
+            "wrap://http/https://raw.githubusercontent.com/polywrap/wasm-test-harness/v0.2.1/wrappers/subinvoke/00-subinvoke/implementations/as",
             "add",
             "{\"a\":1,\"b\":1}"
     );
